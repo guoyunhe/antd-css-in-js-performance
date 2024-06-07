3096cf8d9b974371b9fc70536a6866ab3116e5f9
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-07 16:47:42 +0800

    no strict

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import './antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <StyleProvider hashPriority="high">
-      <App />
-    </StyleProvider>
-  </React.StrictMode>
+  <StyleProvider hashPriority="high">
+    <App />
+  </StyleProvider>
 );
