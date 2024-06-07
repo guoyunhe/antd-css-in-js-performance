819a71d2ddcda2f986e03ef9076a7f2805bd3e40
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-07 13:36:35 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tabs } from "antd";
+import { Form, Select, Button, Table } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form>
+        <Form.Item>
+          <Select />
+        </Form.Item>
+        <Button type="primary">Button</Button>
+      </Form>
+      <Table/>
+      <Tabs/>
     </div>
   );
 }
